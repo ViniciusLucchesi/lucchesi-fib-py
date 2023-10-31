152af91e5580a55187c3419bbc40da80acf7cdda
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-10-30 22:10:25 -0300

    adding setup to module

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lucchesi_fib_py",
+    version="0.0.1",
+    author="Vinicius Lucchesi",
+    author_email="viniciuslucchesi.ti@gmail.com",
+    description="Calculates a Fibonacci number",
+    long_description="A basic library that calculates Fibonacci numbers",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ViniciusLucchesi/lucchesi-fib-py.git",
+    install_requires=[],
+    packages=find_packages(exclude=("tests"),),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    tests_require=['pytest'],
+)
