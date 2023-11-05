c4d1d507720442a96334dde37e1b8e57966bee75
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-11-05 13:00:17 -0300

    Fixing Fibonacci calculation error

--- lucchesi_fib_py/fib_calcs/fib_number.py
+++ lucchesi_fib_py/fib_calcs/fib_number.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 def recurring_fibonacci_number(number: int) -> Optional[int]:
-    if number <= 0:
+    if number < 0:
         return None
     elif number <= 1:
         return number
